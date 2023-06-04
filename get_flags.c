b29438eeefc8065c8003a28ff461c8e5d7c26ccc
Dararo <128896314+Dararo@users.noreply.github.com>
2023-06-05 00:50:03 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * get_flags - It calculates the active flags
+ * @format: A Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *format, int *a)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int b, curr_a;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (b = 0; FLAGS_CH[b] != '\0'; b++)
+			if (format[curr_a] == FLAGS_CH[b])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[b];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[b] == 0)
 			break;
 	}
 
